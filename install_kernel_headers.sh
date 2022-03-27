bfeca0c1b73b78f3b0e03f0675634fe1387a8cfd
Austin Pan <austinpan8@gmail.com>
2022-03-27 16:10:40 +0100

    kernel header installation script

--- install_kernel_headers.sh
+++ install_kernel_headers.sh
@@ -7,6 +7,10 @@
 # Approach taken from https://www.tecmint.com/install-kernel-headers-in-ubuntu-and-debian/
 # Check if kernel headers are installed with
 #   ls -l /usr/src/linux-headers-$(uname -r)
+#
+# It might also be possible to fix the issue by reinstalling the drivers installed
+# by Google's DeepLearning VM through their own scripts:
+#   bash /opts/deeplearning/uninstall-driver.sh && bash /opts/deeplearning/install-driver.sh
 
 sudo apt update
 sudo apt install linux-headers-$(uname -r)
